3cd11498f692efc2e90223dc2e3d155be1e23861
ninjacoder88 <tom@ninjasoft.io>
2025-10-06 16:04:46 -0500

    adding click

--- src/main.py
+++ src/main.py
@@ -2,9 +2,17 @@ import turtle as t
 import time
 import random
 
+def playGame:
+    t.write("Are you ready?", True, ("Arial", 24, "normal"))
+
+onclick(playGame)
+
 t.screensize(1000, 1000)
 t.setup(1000, 1000, None, None)
 
 t.speed(10)
 
-t.write("Start", True, "center", ("Arial", 24, "normal"))
+t.write("Click to Start", True, "center", ("Arial", 24, "normal"))
+
+
+t.mainloop()
