f9cad036b75603ad0750a3b7499350a6bbc804b1
ninjacoder88 <tom@ninjasoft.io>
2025-10-06 16:09:06 -0500

    set up start

--- src/main.py
+++ src/main.py
@@ -2,17 +2,24 @@ import turtle as t
 import time
 import random
 
-def playGame:
+def playGame():
+    t.begin_fill("orange")
+    t.goto(100, 100)
+    t.circle(50)
+    t.end_fill()
     t.write("Are you ready?", True, ("Arial", 24, "normal"))
 
-onclick(playGame)
+    t.begin_fill("orange")
+    t.goto(500, 500)
+    t.circle(50)
+    t.end_fill()
+    t.write("Are you ready?", True, ("Arial", 24, "normal"))
 
-t.screensize(1000, 1000)
+t.screensize(750, 750)
 t.setup(1000, 1000, None, None)
-
 t.speed(10)
 
 t.write("Click to Start", True, "center", ("Arial", 24, "normal"))
-
+onclick(playGame)
 
 t.mainloop()
